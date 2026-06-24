13a1a19d9b3faaccabfb6078bbca9134fd381149
Tanisha Gokhale <tanishagokhale27@gmail.com>
2026-06-24 13:51:59 +0530

    Worked on UI and Bonus Feature Searching

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,7 +9,7 @@ const verifyToken = (req, res, next) => {
     });
   }
 
-  const token = authHeader.split(" ")[1];
+     const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, "secretkey");
